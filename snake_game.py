4f74e3b24547381df4ca3c306eb2dad728170433
unknown <sarasaravevic@gmail.com>
2023-01-29 13:56:57 +0100

    snake head

--- snake_game.py
+++ snake_game.py
@@ -1,10 +1,68 @@
 import turtle
+import time
+
+delay=0.1
+
 
 wn=turtle.Screen()
 wn.title("snake game by@sarsi")
 wn.bgcolor("green")
 wn.setup(width=600,height=600)
 wn.tracer(0)
-wn.mainloop()
  
 #snake head
+head=turtle.Turtle()
+head.speed(0)
+head.shape("square")
+head.color("black")
+head.penup()
+head.goto(0,0)
+head.direction="stop"
+#function
+def go_up():
+    head.direction="up"
+def go_down():
+    head.direction="down"
+def go_left():
+    head.direction="left"
+def go_right():
+    head.direction="right"
+
+
+def move():
+    if head.direction=="up":
+        y=head.ycor()
+        head.sety(y+20)
+    if head.direction=="down":
+        y=head.ycor()
+        head.sety(y-20)
+    if head.direction=="left":
+        x=head.xcor()
+        head.setx(x-20)
+    if head.direction=="right":
+        x=head.xcor()
+        head.setx(x+20)
+
+#keyboard
+
+wn.listen()
+wn.onkeypress(go_up,"w")
+wn.onkeypress(go_down,"s")
+wn.onkeypress(go_left,"a")
+wn.onkeypress(go_right,"d")
+
+
+
+
+
+#main game loop
+while True:
+    wn.update()
+    move()
+
+    time.sleep(delay)
+
+
+
+
+wn.mainloop()

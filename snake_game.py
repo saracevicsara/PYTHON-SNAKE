c0f906f08bccad3dae472a4c882e90adc784d09b
sarsi <sarasaravevic@gmail.com>
2023-02-18 14:04:18 +0100

    adding segments

--- snake_game.py
+++ snake_game.py
@@ -29,6 +29,7 @@ food.penup()
 food.goto(0,100)
 
 segment=[]
+segments=[]
 # pen=turtle.Turtle()
 # pen.shape("square")
 # pen.color("grey")
@@ -74,9 +75,21 @@ wn.onkeypress(go_right,"d")
 #main game loop
 while True:
     wn.update()
+
+    if head.xcor()>290 or head.xcor()<-290 or head.ycor()>290 or head.ycor()<-290:
+        time.sleep(1)
+        head.goto(0,0)
+        head.direction="stop"
+
+        for segment in segments:
+            segment.goto(1000,1000)
+
+        segments.clear()
+
+
     if head.distance(food)<20:
-        x=random.randint(-290,200)
-        y=random.randint(-290,200)
+        x=random.randint(-290,290)
+        y=random.randint(-290,290)
         food.goto(x,y)
         
         new_segment=turtle.Turtle()
@@ -85,6 +98,18 @@ while True:
         new_segment.color("grey")
         new_segment.penup()
         segment.append(new_segment)
+     
+    for index in range(len(segments)-1,0,-1):
+        x=segments[index-1].xcor()
+        y=segments[index-1].ycor()
+        segments[index].goto(x,y)
+
+    if len(segments)>0:
+        x=head.xcor()
+        y=head.ycor()
+        segments[0].goto(x,y)
+
+
     move()
 
     time.sleep(delay)
